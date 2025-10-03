2920a4bc8b670f005fa967f2bcda62810bb82f45
Liam-Gareau <2503580@spsd.sk.ca>
2025-10-03 09:35:05 -0600

    fixed my color scrolling, and added end of game message

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -3,8 +3,9 @@
 // Oct 3 2025
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// I fiugred out how to make the scroll wheel when scroll up and down shufle through a list of colors to change the color of your ball
 
+//All of my variables
 let timer = 0;
 let points = 0;
 let x;
@@ -14,60 +15,75 @@ let direction = " ";
 let colors = ["red","orange","yellow","green","blue","purple"];
 let colorIndex = 0;
 
-
-function setup() {
+//Setup step
+function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  //background(220);
+//Draw step
+function draw(){
   textSize(12);
   drawCircle();
-  changeColor();
+  //changeColor();
 }
 
+//Main function that spawns in the circles and counts the time of the game so that it stops after 60 seconds
 function drawCircle(){
   for (let t = timer; t <= 60; t++){
     if (frameCount % 60 === 0){
       clear();
       randomXandY();
+      changeColor();
       fill(colors[colorIndex]);
       circle(x,y,50);
       timer++;
+      direction = "middle";
     }
     return;
   }
   if (timer > 60) {
     clear();
-    text(points, width / 2, height / 2);
+    text("Congrats, You got " + points + " Points!", width / 2, height / 2);
   }
 }
 
+//Added function to randomize x and y values where their needed
 function randomXandY(){
   x = random(width);
   y = random(height);
 }
 
+//Ask's if the mouse is on the circle and if clicked gives you point
+//It's made so that the more you click on the ball the more points are rewarded so that you arent hard capped at a max of 60 points
 function mouseClicked() {
   if (mouseX < x + radius && mouseX > x - radius && mouseY < y + radius && mouseY > y - radius){
     points++;
   }
 }
 
+//Detects if mouse wheel scrolled up or down using state variables
 function mouseWheel(event){
   if (event.delta > 0) {
-    direction = '▲';
+    direction = "up";
   } 
-  else {
-    direction = '▼';
+  else if(event.delta < 0){
+    direction = "down";
+  }
+  else if(event.delta === 0){
+    direction = "middle";
   }
 }
 
+//Detects the current state variable and scrolls through the color list to give you a color
+//Coded so that if you go over the amount of things in the list it loops back to the beginning allowing fo more colors to be added in the future
 function changeColor(){
-  if (direction === "▲"){
+  if (direction === "up"){
     colorIndex = (colorIndex + 1) % colors.length;
   }
-  else if (direction === "▼"){
+  else if (direction === "down"){
     colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+  }
+  else if (direction === "middle"){
+    colorIndex = colorIndex;
   }
 }
