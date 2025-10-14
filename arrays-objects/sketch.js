cec64df2899162a59a0e47d3b336a0aa084e6268
Liam-Gareau <2503580@spsd.sk.ca>
2025-10-14 09:44:08 -0600

    making the perlin noise demo folder

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -1,116 +1,15 @@
 // Arrays and Objects
 // Liam Gareau
-// Oct 26 2025
+// 
+// make game that you can make different molecules and then name them
 //
 // Extra for Experts:
-// I fiugred out how to make the scroll wheel when scroll up and down shufle through a list of colors to change the color of your ball
+// 
 
-//All of my variables
-let timer = 0;
-let points = 0;
-let x;
-let y;
-let radius = 50/2;
-let direction = " ";
-let colors = ["red","orange","yellow","green","blue","purple"];
-let colorIndex = 0;
-let drawStroke = true;
-
-//Setup step
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth,windowHeight);
 }
 
-//Draw step
 function draw(){
-  textSize(12);
-  drawCircle();
-  makeStroke();
-  keyPressed();
-}
-
-//Main function that spawns in the circles and counts the time of the game so that it stops after 60 seconds
-function drawCircle(){
-  for (let t = timer; t <= 60; t++){
-    if (frameCount % 60 === 0){
-      clear();
-      randomXandY();
-      changeColor();
-      fill(colors[colorIndex]);
-      circle(x,y,50);
-      timer++;
-      direction = "middle";
-    }
-    return;
-  }
-  if (timer > 60) {
-    clear();
-    text("Congrats, You got " + points + " Points!", width / 2, height / 2);
-  }
-}
-
-//Added function to randomize x and y values where their needed
-function randomXandY(){
-  x = random(width);
-  y = random(height);
-}
-
-//Ask's if the mouse is on the circle and if clicked gives you point
-//It's made so that the more you click on the ball the more points are rewarded so that you arent hard capped at a max of 60 points
-function mouseClicked() {
-  if (mouseX < x + radius && mouseX > x - radius && mouseY < y + radius && mouseY > y - radius){
-    points++;
-  }
-}
-
-//Detects if mouse wheel scrolled up or down using state variables
-function mouseWheel(event){
-  if (event.delta > 0) {
-    direction = "up";
-  } 
-  else if(event.delta < 0){
-    direction = "down";
-  }
-  else if(event.delta === 0){
-    direction = "middle";
-  }
-}
-
-//Detects the current state variable and scrolls through the color list to give you a color
-//Coded so that if you go over the amount of things in the list it loops back to the beginning allowing fo more colors to be added in the future
-function changeColor(){
-  if (direction === "up"){
-    colorIndex = (colorIndex + 1) % colors.length;
-  }
-  else if (direction === "down"){
-    colorIndex = (colorIndex - 1 + colors.length) % colors.length;
-  }
-  else if (direction === "middle"){
-    colorIndex = colorIndex;
-  }
-}
-
-//Makes the stroke black or have no stroke depending on if drawStroke the boolean is true or false
-function makeStroke(){
-  if (drawStroke){
-    stroke("black");
-  }
-  else{
-    noStroke();
-  }
-}
-
-//Makes the boolean drawStroke true or false when S key is pressed
-function keyPressed(){
-  if (keyCode === 83){
-    drawStroke = !drawStroke;
-  }
-}
-
-function spawnTarget(){
-  let newObject = {
-    x: x,
-    y: y,
-    
-  };
+  background(255);
 }
