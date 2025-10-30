48cbf2613b8fcbdd708d0d06816f72f9a1d09884
Liam-Gareau <2503580@spsd.sk.ca>
2025-10-30 09:52:24 -0600

    gosper gun

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -6,6 +6,7 @@ let grid;
 let rows;
 let cols;
 let autoPlayIsOn = false;
+let gosper;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -54,6 +55,9 @@ function keyPressed() {
   }
   else if (key === "a") {
     autoPlayIsOn = !autoPlayIsOn;
+  }
+  else if (key === "g") {
+    grid = gosper;
   }
 }
 
@@ -141,4 +145,8 @@ function generateEmptyGrid(cols, rows){
     }
   }
   return newGrid;
+}
+
+function preload(){
+  gosper = loadJSON("gosper.json");
 }
