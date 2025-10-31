e407c0b9a308c5dbc8a11ebfcc6d8d929e22231c
Liam-Gareau <2503580@spsd.sk.ca>
2025-10-31 09:44:39 -0600

    expanded on the player grid to start to add an enemy

---
+++ character-in-grid/sketch.js
@@ -0,0 +1,152 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+const CELL_SIZE = 50;
+const OPEN_TILE = 0;
+const IMPASSABLE = 1;
+const PLAYER = 9;
+const ENEMY = 6;
+let grid;
+let rows;
+let cols;
+let thePlayer = {
+  x: 0,
+  y: 0,
+};
+
+function setup() {
+  createCanvas(windowWidth * 0.9, windowHeight* 0.9);
+  cols = Math.floor(width/CELL_SIZE);
+  rows = Math.floor(height/CELL_SIZE);
+  grid = generateRandomGrid(cols, rows);
+
+  //add player to grid
+  grid[thePlayer.y][thePlayer.x] = PLAYER;
+  grid[theEnemy.y][theEnemy.x] = ENEMY;
+}
+
+function draw() {
+  background("blue");
+  displayGrid();
+}
+
+function mousePressed(){
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+
+  //self
+  toggleCell(x,y);
+}
+
+function theEnemy(){
+  let theEnemy = {
+    x: width,
+    y: height,
+  };
+}
+
+function toggleCell(x,y){
+  if (x >= 0 && x < cols && y >= 0 && y < rows){
+    if (grid[y][x] === OPEN_TILE) {
+      grid[y][x] = IMPASSABLE;
+    }
+    else if (grid[y][x] === IMPASSABLE){
+      grid[y][x] = OPEN_TILE;
+    } 
+  }
+}
+
+function keyPressed() {
+  if (key === "r") {
+    grid = generateRandomGrid(cols, rows);
+  }
+  else if (key === "e") {
+    grid = generateEmptyGrid(cols,rows);
+  }
+  else if (key === "w"){
+    movePlayer(thePlayer.x, thePlayer.y - 1);
+  }
+  else if (key === "a") {
+    movePlayer(thePlayer.x - 1, thePlayer.y);
+  }
+  else if (key === "s") {
+    movePlayer(thePlayer.x, thePlayer.y + 1);
+  }
+  else if (key === "d") {
+    movePlayer(thePlayer.x + 1, thePlayer.y);
+  }
+}
+
+function movePlayer(x,y){
+  if (x >= 0 && x < cols && y >= 0 && y < rows && grid[y][x] === OPEN_TILE){
+    //previous position
+    let oldx = thePlayer.x;
+    let oldy = thePlayer.y;
+  
+    //moving the player location
+    thePlayer.x = x;
+    thePlayer.y = y;
+  
+    //put player on grid
+    grid[thePlayer.y][thePlayer.x] = PLAYER;
+  
+    //reset old spot to be open tile
+    grid[oldy][oldx] = OPEN_TILE;
+  }
+}
+
+function moveEnemy(){
+
+}
+
+function generateRandomGrid(cols, rows){
+  let newGrid = [];
+  for (let y = 0; y < rows; y++){
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++){
+      //pick 0 or 1 randomly
+      if (random(100) < 50){
+        newGrid[y].push(OPEN_TILE);
+      }
+      else {
+        newGrid[y].push(IMPASSABLE);
+      }
+    }
+  }
+  return newGrid;
+}
+
+function displayGrid(){
+  for (let y = 0; y < rows; y++){
+    for (let x = 0; x < cols; x++){
+      if (grid[y][x] === OPEN_TILE){
+        fill(255);
+      }
+      else if (grid[y][x] === IMPASSABLE){
+        fill(0);
+      }
+      else if (grid[y][x] === PLAYER) {
+        fill("green");
+      }
+      else if (grid[y][x] === ENEMY){
+        fill("red");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
+  }
+}
+
+function generateEmptyGrid(cols, rows){
+  let newGrid = [];
+  for (let y = 0; y < rows; y++){
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++){
+      newGrid[y].push(OPEN_TILE);
+    }
+  }
+  return newGrid;
+}
